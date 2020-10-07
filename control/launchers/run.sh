bf5d13337053f17d85246a47099b028ac836ffa5
Liam Paull <paulll@iro.umontreal.ca>
2020-10-07 01:10:52 -0400

    updating the control exercise

--- control/launchers/run.sh
+++ control/launchers/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+source /environment.sh
+source /opt/ros/noetic/setup.bash
+source /code/catkin_ws/devel/setup.bash
 source /code/exercise_ws/devel/setup.bash
 python3 solution.py &
-roslaunch --wait duckietown_demos lane_following.launch
+roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME &
+roslaunch --wait duckietown_demos lane_following.launch &
+
+sleep 5
+if [ "$1" == "--start" ]; then
+  rostopic pub /$VEHICLE_NAME/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "LANE_FOLLOWING"}'
+fi
