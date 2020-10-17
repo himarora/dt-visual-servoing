eb2c237ed3ad109d682ea429c951f3e41168469e
Liam Paull <paulll@iro.umontreal.ca>
2020-10-17 15:13:55 -0400

    updating launchers

--- template_ros/launchers/run_interface.sh
+++ template_ros/launchers/run_interface.sh
@@ -4,6 +4,5 @@ source /environment.sh
 source /opt/ros/noetic/setup.bash
 source /code/catkin_ws/devel/setup.bash
 source /code/exercise_ws/devel/setup.bash
-python3 solution.py &
+python3 /code/solution.py &
 roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME &
-roslaunch --wait duckietown_demos lane_following.launch
