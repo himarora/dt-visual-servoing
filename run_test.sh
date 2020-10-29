264e6abbcd30dcf2418ec092a56b364225b27d8b
Anthony Courchesne <anthony.courchesne@mail.mcgill.ca>
2020-10-29 15:38:55 -0400

    Add small smoke test

---
+++ run_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Quick smoke test to see if the workflow is working.
+# We start the workflow and check if all the 
+# required container are started.
+
+cd control
+echo "Starting workflow, please wait"
+dts exercises test --sim >/dev/null &
+PROCESSID=$!
+sleep 12
+
+C1=`docker ps | grep duckietown/challenge-aido_lf-baseline-duckietown | wc -l`
+C2=`docker ps | grep duckietown/dt-gui-tools | wc -l`
+C3=`docker ps | grep duckietown/dt-commons | wc -l`
+C4=`docker ps | grep duckietown/mooc-fifos-connector | wc -l`
+C5=`docker ps | grep duckietown/challenge-aido_lf-simulator-gym | wc -l`
+OUTPUT=`docker ps -an5`
+
+echo "Stopping workflow"
+kill $PROCESSID
+
+echo "Removing containers ..."
+dts exercises test --sim --stop >/dev/null
+
+if [ $((C1+C2+C3+C4+C5)) -eq 5 ]; then
+  echo "Test passed !"
+else
+  echo "Some containers have not started successfully:"
+  echo "$OUTPUT"
+  echo "Use docker logs for the failed container"
+fi
